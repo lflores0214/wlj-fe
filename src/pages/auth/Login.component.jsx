91399aa92956a6e83ccb653a82489d67b6aa5fbc
Luis Flores <lflores02142@gmail.com>
2020-08-04 11:30:19 -0700

    added show password functionality and error handling to login component

--- src/pages/auth/Login.component.jsx
+++ src/pages/auth/Login.component.jsx
@@ -9,31 +9,63 @@ import {
   FormLabel,
   Input,
   Button,
+  Text,
+  CircularProgress,
+  InputGroup,
+  InputRightElement,
+  Icon,
 } from "@chakra-ui/core";
+
+import ErrorMessage from "../../components/ErrorMessage";
 
 const LoginForm = () => {
   const [userInfo, setUserInfo] = useState({
     username: "",
     password: "",
   });
+  const [isLoading, setIsLoading] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [error, setError] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
 
-  const handleSubmit = (e) => {
+  const handlePasswordVisibility = () => setShowPassword(!showPassword);
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    axios.post(
-      "https://weightliftingjournal1.herokuapp.com/api/auth/login",
-      userInfo
-    )
-    .then(response => {
+    setIsLoading(true);
+
+    axios
+      .post(
+        "https://weightliftingjournal1.herokuapp.com/api/auth/login",
+        userInfo
+      )
+      .then((response) => {
         console.log(response);
-        alert(userInfo)
-    })
-  };
-  const handleChange = e => {
-    setUserInfo({
-        ...userInfo,
-        [e.target.name]: e.target.value
+        setIsLoggedIn(true);
+        setIsLoading(false);
+        setShowPassword(false);
+        setUserInfo({
+          username: "",
+          password: "",
+        });
       })
-  }
+      .catch((error) => {
+        console.log(error);
+        setError(`Error logging in`);
+        setIsLoading(false);
+        setShowPassword(false);
+        setUserInfo({
+          username: "",
+          password: "",
+        });
+      });
+  };
+  const handleChange = (e) => {
+    setUserInfo({
+      ...userInfo,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   return (
     <Flex width="full" align="center" justifyContent="center">
@@ -50,31 +82,66 @@ const LoginForm = () => {
           borderRadius={8}
           boxShadow="lg"
         >
-          <form onSubmit={handleSubmit}>
-            <FormControl isRequired>
-              <FormLabel>Email</FormLabel>
-              <Input
-                type="email"
-                placeholder="Email"
-                isRequired
-                size="lg"
-                onChange={handleChange}
-              />
-            </FormControl>
-            <FormControl isRequired>
-              <FormLabel>Password</FormLabel>
-              <Input
-                type="password"
-                placeholder="password"
-                isRequired
-                size="lg"
-                onChange={handleChange}
-              />
-            </FormControl>
-            <Button width="full" mt={4} type="submit">
-              Sign In
-            </Button>
-          </form>
+          {isLoggedIn ? (
+            <Box textAlign="center">
+              <Text>{userInfo.username} logged In!</Text>
+              <Button
+                variantColor="orange"
+                variant="outline"
+                width="full"
+                mt={4}
+                onClick={() => setIsLoggedIn(false)}
+              >
+                Sign Out
+              </Button>
+            </Box>
+          ) : (
+            <form onSubmit={handleSubmit}>
+              {error && <ErrorMessage message={error} />}
+              <FormControl isRequired>
+                <FormLabel>Username</FormLabel>
+                <Input
+                  type="text"
+                  placeholder="Username"
+                  isRequired
+                  size="lg"
+                  onChange={handleChange}
+                />
+              </FormControl>
+              <FormControl isRequired>
+                <FormLabel>Password</FormLabel>
+                <InputGroup>
+                  <Input
+                    type={showPassword ? "text" : "password"}
+                    placeholder="password"
+                    isRequired
+                    size="lg"
+                    onChange={handleChange}
+                  />
+                  <InputRightElement width="3rem">
+                    <Button
+                      h="1.5rem"
+                      size="sm"
+                      onClick={handlePasswordVisibility}
+                    >
+                      {showPassword ? (
+                        <Icon name="view-off" />
+                      ) : (
+                        <Icon name="view" />
+                      )}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+              </FormControl>
+              <Button width="full" mt={4} type="submit">
+                {isLoading ? (
+                  <CircularProgress isIndeterminate size="24px" />
+                ) : (
+                  "SignIn"
+                )}
+              </Button>
+            </form>
+          )}
         </Box>
       </Box>
     </Flex>
